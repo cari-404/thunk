7810e088690412673ab125e24cae3a976a5bd744
cari-404 <135954797+cari-404@users.noreply.github.com>
2025-03-08 13:04:22 +0700

    update dependency

--- thunk-rs/src/lib.rs
+++ thunk-rs/src/lib.rs
@@ -2,8 +2,8 @@
 
 use std::{env, path::PathBuf, process::Command};
 
-const VC_LTL_VERSION: &'static str = "5.2.1-Beta4";
-const YY_THUNKS_VERSION: &'static str = "1.1.6-Beta5";
+const VC_LTL_VERSION: &'static str = "5.2.1";
+const YY_THUNKS_VERSION: &'static str = "1.1.6";
 
 /// This function should be call in build.rs.
 pub fn thunk() {
