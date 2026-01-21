24383da1a8ef15d0a06b218f22a72ec845b564b5
cari-404 <135954797+cari-404@users.noreply.github.com>
2026-01-21 12:50:53 +0700

    update VC_LTL = "5.3.1" and YY_THUNKS_VERSION = "1.1.10-Beta1"

--- thunk-rs/src/lib.rs
+++ thunk-rs/src/lib.rs
@@ -2,8 +2,8 @@
 
 use std::{env, path::PathBuf, process::Command};
 
-const VC_LTL_VERSION: &'static str = "5.2.2";
-const YY_THUNKS_VERSION: &'static str = "1.1.8";
+const VC_LTL_VERSION: &'static str = "5.3.1";
+const YY_THUNKS_VERSION: &'static str = "1.1.10-Beta1";
 
 /// This function should be call in build.rs.
 pub fn thunk() {
